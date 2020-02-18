2b4d730745d1004e2068734dd46aebfbcc73fa1a
Jon Morss <jon.morss@someemail.com>
2020-02-18 01:45:38 -0800

    Added move_slower feature to slow player when pressing the Shift key

--- MetalBurstGame/Player.gd
+++ MetalBurstGame/Player.gd
@@ -5,10 +5,19 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-export var speed : float = 100;
+export var speed : float = 200;
+var screen_size      # size of the game window
+var speed_reduced = 0
+
+func start(pos):
+	position = pos
+	$Collisionshape2D.disabled = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	screen_size = get_viewport_rect().size # Replace with function body.
+	
 	set_process(true)
 
 
@@ -19,11 +28,15 @@ func _process(delta):
 	if Input.is_action_pressed("fire"):
 		print("firing")
 	if Input.is_action_pressed("move_up"):
-		move.y-=speed 
+		move.y-=speed + speed_reduced
 	if Input.is_action_pressed("move_down"):
-		move.y+=speed
+		move.y+=speed + speed_reduced
 	if Input.is_action_pressed("move_left"):
-		move.x-=speed
+		move.x-=speed + speed_reduced
 	if Input.is_action_pressed("move_right"):
-		move.x+=speed
+		move.x+=speed + speed_reduced
+	if Input.is_action_pressed("move_slower"):
+		speed_reduced = -150
+	if !(Input.is_action_pressed("move_slower")):
+		speed_reduced = 0
 	translate(move*delta)
