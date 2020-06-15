537b603a834c488f3d98e5b018057c894311cf10
kyperbelt <jonathancamarenacamcho@gmail.com>
2020-06-15 11:25:31 -0700

    added new nodes - Parallel,MoveForward

---
+++ MetalBurstGame/Prefabs/System/Behavior/Actions/Movement/MoveForward.gd
@@ -0,0 +1,25 @@
+extends Action
+# move the entity towards its forward direction - this is related to its 
+# angle
+## This requires that the owner is a Node2D or/and has the following functions:
+## get_rotation() function 
+## set_velocity(x,y)
+
+class_name MoveForward
+
+var _failed : bool = false #check to see if method is present
+var _self = null
+
+func initiate():
+	.initiate()
+	_self = get_blackboard()[BB.SELF]
+	_failed = !_self.has_method("get_rotation")
+
+func _update_behavior(delta:float)->int:
+	
+	var r : float = _self.get_rotation()+PI/2
+	print("rotation: %s" % r)
+	var x : float = cos(r)
+	var y : float = sin(r)
+	_self.set_velocity(x,y)
+	return RunState.Success
