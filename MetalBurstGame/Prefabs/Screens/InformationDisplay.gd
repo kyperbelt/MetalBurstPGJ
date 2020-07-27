2ec0876f3d90af168775e50d0e60a8d3dd846437
kyperbelt <jonathancamarenacamcho@gmail.com>
2020-07-26 21:21:26 -0700

    first pass multiplier

--- MetalBurstGame/Prefabs/Screens/InformationDisplay.gd
+++ MetalBurstGame/Prefabs/Screens/InformationDisplay.gd
@@ -7,15 +7,17 @@ func _ready():
 	#placeholder signal connection :::
 	# we want to connect the singals from individual components 
 	# later, like the bomb energy progress bar - boss hp ect
-	var _ok = connect("lives_changed",self,"update_lives")
-	_ok = connect("score_changed",self,"update_score")
-	_ok = connect("bombs_changed",self,"update_bombs")
+	var _ok = 	connect("lives_changed",self,"update_lives")
+	_ok = 		connect("score_changed",self,"update_score")
+	_ok =	 	connect("bombs_changed",self,"update_bombs")
+	_ok = 		connect("multiplier_changed",self,"update_multiplier")
 	pass 
 
 signal lives_changed
 signal score_changed
 signal bombs_changed
 signal stage_name_changed
+signal multiplier_changed
 
 func set_lives(lives:int,increment=true):
 	emit_signal("lives_changed",lives,increment)
@@ -33,6 +35,9 @@ func set_bombs(bombs:int,increment=true):
 func set_stage_name(name:String):
 	emit_signal("stage_name_changed",name)
 	pass
+
+func set_multiplier(mult:int):
+	emit_signal("multiplier_changed",mult)
 
 
 ##################################
@@ -54,3 +59,6 @@ func update_bombs(bombs,_increment):
 
 func update_name(name):
 	$StageName.set_text(name)
+
+func update_multiplier(mult:int):
+	$MultiplierContainer/Multiplier.set_text("X%s"%mult);
